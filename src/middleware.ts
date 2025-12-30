350b5af6845037c4deb7bc311fce0ba6e50613b5
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2025-12-30 12:30:59 +0100

    Disable middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,17 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-import { Routes } from "./lib/routes/routes";
+// import { Routes } from "./lib/routes/routes";
 
-export function middleware(request: NextRequest) {
-  const refreshToken = request.cookies.get("refresh_token")?.value;
+// export function middleware(request: NextRequest) {
+//   const refreshToken = request.cookies.get("refresh_token")?.value;
 
-  if (!refreshToken) {
-    return NextResponse.redirect(new URL(Routes.signIn(), request.url));
-  }
-  return NextResponse.next();
-}
+//   if (!refreshToken) {
+//     return NextResponse.redirect(new URL(Routes.signIn(), request.url));
+//   }
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: "/dashboard/:path*",
-};
+// export const config = {
+//   matcher: "/dashboard/:path*",
+// };
